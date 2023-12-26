8a3f09dd104eb2bb4287faf9391f6fc983d047a1
Marcelo Baraldi <mlbaraldi@hotmail.com>
2023-12-26 11:08:40 -0300

    feat:custom 404 and fix footer

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
+
+const NotFound = () => {
+  return (
+    <main className="text-center p-10 text-xl">
+      <h2 className="text-xl md:text-3xl font-bold p-auto mt-4 p-5">
+        404 - Oops. You were not supposed to be here!
+      </h2>
+      <h3>
+        but hey, life is an adventure.</h3>
+      <div className=" p-10">
+        <Link
+          className={buttonVariants({
+            variant: 'ghost',
+            size: 'lg',
+          })} href='/' passHref>
+          Return Home
+        </Link>
+      </div>
+
+    </main>
+  )
+}
+
+
+export default NotFound
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,7 +4,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 export default function Footer() {
   return (
     <>
-      <footer className='sticky p-4 top-0 w-full  bg-black dark:bg-opacity-40 bg-opacity-10 border-b backdrop-blur-lg transition-all '>
+      <footer className='sticky p-4  top-[100vh] w-full  bg-black dark:bg-opacity-40 bg-opacity-10 border-b backdrop-blur-lg transition-all '>
         <MaxWidthWrapper>
           <div className="flex flex-row items-center justify-center">
             <Mail />
