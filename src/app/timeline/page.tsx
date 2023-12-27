e5b1c746c4a38575cf63e4f467c5f4091a26eeec
Marcelo Baraldi <mlbaraldi@hotmail.com>
2023-12-26 21:49:40 -0300

    feat: add timeline page

---
+++ src/app/timeline/page.tsx
@@ -0,0 +1,31 @@
+
+const Works = () => {
+  return (
+    <div className="mx-auto max-w-2xl p-12">
+      <h1 className="text-3xl font-bold mb-4 text-center p-5">TIMELINE - a brief summary</h1>
+      <div className="text-justify font-medium leading-loose text-lg">
+        <p>
+          Marcelo Baraldi was born in Brazil in 1989. Since his childhood, he has displayed a keen interest in technology, spending hours exploring and learning on the command lines of his father's personal computer, an Intel 286. This early affinity for technology often placed him in front of the computer, where he routinely tackled and solved problems.
+        </p>
+
+        <p>
+          At the age of 18, Marcelo embarked on an academic journey at UFSC (Federal University of Santa Catarina), graduating in Cinema in 2012. This experience broadened his horizons, providing him with a unique perspective on the world and instilling the belief that both objective and subjective knowledge contribute to the creation of new functionalities or insights.
+        </p>
+
+        <p>
+          His path in software development took a new turn in 2019 when he enrolled in the undergraduate program in Systems Analysis and Development at PUC-PR. This marked his return to the academic environment and fueled his quest for knowledge in the technology field.
+        </p>
+
+        <p>
+          In 2020, Marcelo entered a new phase by joining Tecnospeed, a company where he continues to work to this day. Playing a pivotal role in backend development, he leverages various technologies such as Delphi, NodeJS, TypeScript, and AWS services.
+        </p>
+
+        <p>
+          As time has passed and his career has matured, Marcelo is focused on specializing as a FullStack Developer. Currently, he is engaged in projects utilizing modern technologies like Next, TypeScript, Prism, gRPC, and Tailwind, showcasing his dedication to ongoing professional improvement.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
