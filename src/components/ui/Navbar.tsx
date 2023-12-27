34c6f7a05e3314a8ecb750319b1cb09c088dff30
Marcelo Baraldi <mlbaraldi@hotmail.com>
2023-12-26 21:19:50 -0300

    refactor: decompose navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -6,6 +6,37 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { buttonVariants } from "./button";
 
+const components: { title: string; href: string; description: string, icon: LucideIcon }[] = [
+  {
+    title: "Works",
+    href: "/works",
+    description:
+      "Let me show you some stuff.",
+    icon: <Terminal className='ml-1.5 h-5 w-5' />
+  },
+  {
+    title: "Timeline",
+    href: "/timeline",
+    description:
+      "What was I up to.",
+    icon: <LineChartIcon className='ml-1.5 h-5 w-5' />
+  },
+  {
+    title: "Linkedin",
+    href: "/https://www.linkedin.com/in/mlbaraldi",
+    description:
+      "My Linkedin page.",
+    icon: <LinkedinIcon className='ml-1.5 h-5 w-5' />
+  },
+  {
+    title: "Github",
+    href: "https://github.com/mlbaraldi",
+    description:
+      "My github page.",
+    icon: <GithubIcon className='ml-1.5 h-5 w-5' />
+  }
+]
+
 const Navbar = () => {
   return (
     <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b backdrop-blur-lg transition-all '>
@@ -13,37 +44,25 @@ const Navbar = () => {
         <div className='flex items-center justify-between h-14 border-b '>
           <BaseIcon />
           <div className='hidden items-center space-x-20 xl:flex'>
-            <Link className={buttonVariants({
-              variant: 'ghost',
-              size: 'sm'
-            })} href='/works' passHref>
-              <Terminal />
-              <p className="p-2">Works</p>
-            </Link>
-            <Link
-              className={buttonVariants({
-                variant: 'ghost',
-                size: 'sm',
-              })} href='/timeline' passHref>
-              <LineChartIcon className='ml-1.5 h-5 w-5' />
-              <p className="p-2">Timeline</p>
-            </Link>
-            <Link
-              className={buttonVariants({
-                variant: 'ghost',
-                size: 'sm',
-              })} href='https://www.linkedin.com/in/mlbaraldi' passHref target="_blank">
-              <LinkedinIcon className='ml-1.5 h-5 w-5' />
-              <p className="p-2">Linkedin</p>
-            </Link>
-            <Link
-              className={buttonVariants({
-                variant: 'ghost',
-                size: 'sm',
-              })} href='https://github.com/mlbaraldi' passHref target="_blank">
-              <GithubIcon className='ml-1.5 h-5 w-5' />
-              <p className="p-2">Github</p>
-            </Link>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-4 lg:w-[600px] ">
+              {components.map((component) => (
+                <li key={component.title}>
+                  <Link
+                    title={component.description}
+                    href={component.href}
+                    passHref
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm'
+                    })}
+                  >
+                    {component.icon}
+                    <p className="p-2">{component.title}</p>
+
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
 
           {/* Theme switcher for all screens */}
