0135327fa80be6253443cbde1ff4474b11e05236
Marcelo Baraldi <mlbaraldi@hotmail.com>
2023-12-26 21:51:39 -0300

    feat: add works page

---
+++ src/app/works/page.tsx
@@ -0,0 +1,14 @@
+const works = () => {
+  return (
+    <div className="mx-auto max-w-2xl p-12">
+      <h1 className="text-3xl font-bold mb-4 text-center p-5">Works - the road so far</h1>
+      <div className="text-justify font-medium leading-loose text-lg">
+        <p>
+          Here I'll show some of my side projets. Stay tuned!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default works;
